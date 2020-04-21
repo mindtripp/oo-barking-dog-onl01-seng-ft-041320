ccf446c6430b462e4968773ab5e7b036993343ba
decent-scraper-1450 <jaimealba1574@gmail.com>
2020-04-21 14:56:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+# Your class Dog
+  def name
+    @name
+  end
+
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  def bark
+    puts "woof!"
+  end
+end
+
+# class Dog
+
+   # def bark
+   #   puts "woof!"
+   # end
+
+# end goes here!
